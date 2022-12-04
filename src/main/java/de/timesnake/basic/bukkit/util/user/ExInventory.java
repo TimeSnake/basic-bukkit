eeaa1c76d336f5fc4030949d382089e7c6118b98
funzter <funzter@mail.timesnake.de>
2022-12-04 22:05:35 +0100

    removed ExInventory interface, cleanup

--- src/main/java/de/timesnake/basic/bukkit/util/user/ExInventory.java
+++ src/main/java/de/timesnake/basic/bukkit/util/user/ExInventory.java
@@ -1,5 +1,5 @@
 /*
- * basic-bukkit.main
+ * workspace.basic-bukkit.main
  * Copyright (C) 2022 timesnake
  *
  * This program is free software; you can redistribute it and/or
@@ -18,22 +18,66 @@
 
 package de.timesnake.basic.bukkit.util.user;
 
+import de.timesnake.basic.bukkit.core.user.inventory.InventoryDelegation;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
-public interface ExInventory {
+public class ExInventory extends InventoryDelegation {
 
-    String getTitle();
+    public ExInventory(@NotNull Inventory inventory) {
+        super(inventory);
+    }
 
-    Integer getSize();
+    public ExInventory(int size, Component title, ExItemStack... itemStacks) {
+        size = (int) (Math.ceil(size / 9.0) * 9);
+        this.inventory = Bukkit.createInventory(null, size, title);
+        for (ExItemStack itemStack : itemStacks) {
+            this.setItemStack(itemStack);
+        }
+    }
 
-    void setItemStack(int index, Material material, int amount, String name, List<String> lore);
+    public ExInventory(int size, Component title, InventoryHolder holder, ExItemStack... itemStacks) {
+        size = (int) (Math.ceil(size / 9.0) * 9);
+        this.inventory = Bukkit.createInventory(holder, size > 0 ? size : 9, title);
+        for (ExItemStack itemStack : itemStacks) {
+            this.setItemStack(itemStack);
+        }
+    }
 
-    void setItemStack(int index, org.bukkit.inventory.ItemStack item);
+    @Deprecated
+    public ExInventory(int size, String title, ExItemStack... itemStacks) {
+        this(size, LegacyComponentSerializer.legacyAmpersand().deserialize(title), itemStacks);
+    }
 
-    void setItemStack(ExItemStack item);
+    @Deprecated
+    public ExInventory(int size, String title, InventoryHolder holder, ExItemStack... itemStacks) {
+        this(size, LegacyComponentSerializer.legacyAmpersand().deserialize(title), holder, itemStacks);
+    }
+
+    public void setItemStack(int index, Material material, int amount, String name, List<String> lore) {
+        this.inventory.setItem(index, new ExItemStack(material, amount, name, lore));
+    }
+
+    public void setItemStack(int index, org.bukkit.inventory.ItemStack item) {
+        this.inventory.setItem(index, item);
+    }
+
+    public void setItemStack(ExItemStack item) {
+        if (item.getSlot() != null) {
+            this.inventory.setItem(item.getSlot(), item);
+        } else {
+            this.inventory.addItem(item);
+        }
+    }
 
     /**
      * Removes an {@link ExItemStack} from inventory by id
@@ -41,15 +85,41 @@ public interface ExInventory {
      * @param item The {@link ExItemStack} to remove
      * @return if removed the slot, else null
      */
-    Integer removeItemStack(ExItemStack item);
+    public Integer removeItemStack(ExItemStack item) {
+        for (int slot = 0; slot < this.getInventory().getSize(); slot++) {
+            ItemStack i = this.getInventory().getItem(slot);
+            if (i == null) {
+                continue;
+            }
 
-    void removeItemStack(int index);
+            ExItemStack exItem = ExItemStack.getItem(i, true);
 
-    void update();
+            if (exItem.equals(item)) {
+                this.inventory.setItem(slot, null);
+                return slot;
+            }
+        }
+        return null;
+    }
 
-    Integer getFirstEmptySlot();
+    public void removeItemStack(int index) {
+        this.inventory.clear(index);
+    }
 
-    Integer getFirstEmptySlot(int begin);
+    public void update() {
+        this.inventory.getViewers().forEach((h -> ((Player) h).updateInventory()));
+    }
 
-    Inventory getInventory();
+    public Integer getFirstEmptySlot() {
+        return this.getFirstEmptySlot(0);
+    }
+
+    public Integer getFirstEmptySlot(int begin) {
+        for (int i = begin; i < this.inventory.getSize(); i++) {
+            if (this.inventory.getItem(i) == null) {
+                return i;
+            }
+        }
+        return null;
+    }
 }
