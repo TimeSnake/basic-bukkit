8ea8b0d497b0166b296c5f615766a176c0cfdca9
funzter <cavehive@protonmail.com>
2022-12-18 18:41:19 +0100

    minor

--- src/main/java/de/timesnake/basic/bukkit/util/chat/Plugin.java
+++ src/main/java/de/timesnake/basic/bukkit/util/chat/Plugin.java
@@ -20,12 +20,11 @@ package de.timesnake.basic.bukkit.util.chat;
 
 import de.timesnake.library.basic.util.LogHelper;
 
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Plugin extends de.timesnake.library.extension.util.chat.Plugin {
 
-    public static final Plugin WORLDS = new Plugin("Worlds", "BSW", LogHelper.getLogger("Worlds", Level.INFO));
+    public static final Plugin WORLDS = new Plugin("Worlds", "BSW", LogHelper.getLogger("Worlds"));
     public static final Plugin PACKETS = new Plugin("Packets", "BPS", LogHelper.getLogger("Packets"));
 
     protected Plugin(String name, String code, Logger logger) {
